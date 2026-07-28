be35fa16bea717b5cd1537fcae7eea326880349b
Joviank <Joviak@hotmail.no>
2026-07-28 12:08:54 +0200

    first commit

---
+++ TaskService.test/TodoTest.cs
@@ -0,0 +1,37 @@
+﻿namespace TaskService.test;
+
+public class TodoTest
+{
+    [Fact]
+    public void AddTask_ShouldAddTaskToDatabase()
+    {
+        // Arrange
+        var task = new TaskService();
+
+        //Act
+        task.AddTask("Laundry");
+
+        //Assert
+        Assert.Single(task.GetTasks());
+    }
+
+    [Fact]
+    public void CompleteTask_ShouldMarkTaskAsCompleted()
+    {
+        // Given
+        var taskClass = new TaskService();
+        var task = taskClass.AddTask("Wake up");
+    
+        // When
+        taskClass.CompleteTask(task.Id);
+    
+        // Then
+        Assert.True(taskClass.GetTasks().First().IsCompleted);
+    }
+
+    [Fact]
+    public void DeleteTask_ShouldRemoveTask()
+    {
+       
+    }
+}
---
+++ TaskService/TaskService.cs
@@ -0,0 +1,31 @@
+﻿namespace TaskService;
+
+public class TaskService
+{
+    private readonly List<TaskItem> tasks = new();
+    private int nextId = 1;
+
+    public TaskItem AddTask(string title)
+    {
+        var task = new TaskItem(nextId++, title);
+        tasks.Add(task);
+        return task;
+    }
+        public List<TaskItem> GetTasks()
+    {
+        return tasks.ToList();
+    }
+
+    public void CompleteTask (int id)
+    {
+        var task = tasks.FirstOrDefault(t => t.Id == id);
+
+        if (task == null)
+        {
+            throw new KeyNotFoundException("Task was not found");
+        }
+
+        task.IsCompleted = true;
+    }
+
+}
